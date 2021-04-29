4410984e30b83194f5b9fc4e95e5eccb34362bb7
Kristin Ponsonby <kristinponsonby@gmail.com>
2021-04-28 20:23:32 -0400

    commit tree c1615745e32d1dadc6717d8403f3583dfbef8bde

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app!!!! HURRAH I BUILT AN APP!! WOW LOOK IT STILL WORKS COOOOOOL <3 "
   end
 
 end
